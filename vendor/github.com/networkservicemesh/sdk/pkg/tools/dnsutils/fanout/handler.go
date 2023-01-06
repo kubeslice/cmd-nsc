a26cf48a0cfff8eb429b7d5a7b8cf6d46345b9eb
bharath-avesha <99859949+bharath-avesha@users.noreply.github.com>
2023-01-06 15:26:59 +0530

    fix(): Fixed DNS resolution (#2)
    Added support for dns responses that contain a success return code but empty
    ANSWER sections.
    
    Signed-off-by: Bharath Horatti <bharath@aveshasystems.com>
    
    Signed-off-by: Bharath Horatti <bharath@aveshasystems.com>

--- vendor/github.com/networkservicemesh/sdk/pkg/tools/dnsutils/fanout/handler.go
+++ vendor/github.com/networkservicemesh/sdk/pkg/tools/dnsutils/fanout/handler.go
@@ -74,7 +74,19 @@ func (h *fanoutHandler) ServeDNS(ctx context.Context, rw dns.ResponseWriter, msg
 	var resp = h.waitResponse(ctx, responseCh)
 
 	if resp == nil {
-		dns.HandleFailed(rw, msg)
+		m := new(dns.Msg)
+		// TODO: The waitResponse() func needs to be improved to return the correct error code if none of the
+		// queried nameservers return an answer. We need a way to aggregate the error codes and choose what error
+		// to write in the dns response message if different nameservers returned different error codes.
+		// Return NXDOMAIN error code by default if resp is empty. Returning SERVFAIL is problematic because it
+		// means the server does not want to return an answer due to an internal error or similar errors. It
+		// indicates that the server might have the answer but does not want to return it. Many apps seem to treat this
+		// as a blanket dns server failure, leading to unexpected side effects.
+		m.SetRcode(msg, dns.RcodeNameError)
+		err := rw.WriteMsg(m)
+		if err != nil {
+			log.FromContext(ctx).WithField("fanoutHandler", "ServeDNS").Warnf("Error writing NameError: %v", err.Error())
+		}
 		return
 	}
 
@@ -104,11 +116,11 @@ func (h *fanoutHandler) waitResponse(ctx context.Context, respCh <-chan *dns.Msg
 			}
 			if resp.Rcode == dns.RcodeSuccess {
 				if len(resp.Answer) == 0 {
-                                        if respCount == 0 {
-                                                return nil
-                                        }
-                                        continue
-                                }
+					if respCount == 0 {
+						return resp
+					}
+					continue
+				}
 				return resp
 			}
 			if respCount == 0 {
