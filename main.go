e65245d1e2383d74feb7a85d8e2a65d7f2356344
Bharath Horatti <bharath@aveshasystems.com>
2023-06-14 07:08:07 +0000

    fix(): Support tcp connection to nsmgr
    Fixed code formatting.
    
    Signed-off-by: Bharath Horatti <bharath@aveshasystems.com>

--- main.go
+++ main.go
@@ -86,66 +86,68 @@ import (
 	"github.com/networkservicemesh/cmd-nsc/internal/config"
 )
 
+func getResolverAddress() (string, error) {
+	file, err := os.Open("/etc/nsm-dns-config/resolv.conf.restore")
+	if err != nil {
+		return "", err
+	}
 
+	resolverAddr := ""
 
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
 
-func getResolverAddress () (string, error) {
-       file, err := os.Open("/etc/nsm-dns-config/resolv.conf.restore")
-       if err != nil {
-               return "", err
-       }
+	for scanner.Scan() {
+		cfgLine := strings.Split(scanner.Text(), " ")
+		if cfgLine[0] == "nameserver" {
+			resolverAddr = cfgLine[1]
+			break
+		}
+	}
 
-       resolverAddr := ""
-
-       scanner := bufio.NewScanner(file)
-       scanner.Split(bufio.ScanLines)
-
-       for scanner.Scan() {
-               cfgLine := strings.Split(scanner.Text(), " ")
-               if cfgLine[0] == "nameserver" {
-                       resolverAddr = cfgLine[1]
-                       break
-               }
-       }
-
-       return resolverAddr, nil
+	return resolverAddr, nil
 }
 
-func resolveNsmConnectURL (ctx context.Context, connectURL *url.URL) (string, error) {
-       if connectURL.Scheme == "unix" {
-               return connectURL.Host, nil
-       }
+func resolveNsmConnectURL(ctx context.Context, connectURL *url.URL) (string, error) {
+	if connectURL.Scheme == "unix" {
+		return connectURL.Host, nil
+	}
 
-       resolverAddr, err := getResolverAddress()
-       if err != nil {
-               return "", err
-       }
+	// The resolv.conf is overwritten before the connection to nsmgr is established. This turns into
+	// a chicken and egg problem. Until the connection to nsmgr is established and the nsc is
+	// receives connection context to the nse, the dns proxy would not know the IP address of the
+	// upstream dns servers, hence it cannot resolve any dns names. To fix this problem, we will read the
+	// IP address of kube-dns service backed up in /etc/nsm-dns-config/resolv.conf.restore and use it to
+	// resolve the tcp connect URL.
+	resolverAddr, err := getResolverAddress()
+	if err != nil {
+		return "", err
+	}
 
-       resolver := net.Resolver{
-               PreferGo: true,
-               Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
-                       dialer := net.Dialer{}
-                       return dialer.DialContext(ctx, "udp", net.JoinHostPort(resolverAddr, "53"))
-               },
-       }
+	resolver := net.Resolver{
+		PreferGo: true,
+		Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
+			dialer := net.Dialer{}
+			return dialer.DialContext(ctx, "udp", net.JoinHostPort(resolverAddr, "53"))
+		},
+	}
 
-       host, port, err := net.SplitHostPort(connectURL.Host)
-       if err != nil {
-	       return "", err
-       }
+	host, port, err := net.SplitHostPort(connectURL.Host)
+	if err != nil {
+		return "", err
+	}
 
-       addrs, err := resolver.LookupHost(ctx, host)
-       if err != nil {
-               return "", err
-       }
+	addrs, err := resolver.LookupHost(ctx, host)
+	if err != nil {
+		return "", err
+	}
 
-       if len(addrs) == 0 {
-               return "", errors.New("error resolving connect URL, addr list empty")
-       }
+	if len(addrs) == 0 {
+		return "", errors.New("error resolving connect URL, addr list empty")
+	}
 
-       return net.JoinHostPort(addrs[0], port), nil
+	return net.JoinHostPort(addrs[0], port), nil
 }
-
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
@@ -178,12 +180,12 @@ func main() {
 	}
 	logrus.SetLevel(level)
 
+	// Resolve connect URL if the connection scheme is tcp or udp
 	resolvedHost, err := resolveNsmConnectURL(ctx, &c.ConnectTo)
-        if err != nil {
-                logrus.Fatalf("error resolving nsm connect host: %v, err: %v", c.ConnectTo, err)
-        }
-        c.ConnectTo.Host = resolvedHost
-
+	if err != nil {
+		logrus.Fatalf("error resolving nsm connect host: %v, err: %v", c.ConnectTo, err)
+	}
+	c.ConnectTo.Host = resolvedHost
 
 	logger.Infof("rootConf: %+v", c)
 
@@ -371,7 +373,6 @@ func main() {
 
 		logger.Infof("successfully connected to %v. Response: %v", u.NetworkService(), resp)
 	}
-
 
 	// Wait for cancel event to terminate
 	<-signalCtx.Done()
